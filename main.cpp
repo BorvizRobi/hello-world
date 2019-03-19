f4a23e1fc61fc4aac9d54a69590b1ab40bd0afca
BorvizRobi <43295711+BorvizRobi@users.noreply.github.com>
2019-03-19 21:38:53 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iosteam>
+#include "mlp.hpp"
+#include <png++/png.hpp>
+
+int main( int argc, char **argv)
+{
+
+png::image <png::rgb_pixel> png_image (argv[1]);
+
+int size = png_image.get_widith() *png_image.get_height();
+
+Perceptron* p =new Perceptron(3,size,256,1);
+
+double* image = new double[size];
+
+for(int i{0}; i<png_image.get_width();++i)
+  for(int j{0}; i<png_image.get_width();++j)
+    image[i*png_image.get_width()+j]=png_image[i][j].red;
+
+double value =(*p)(image);
+
+std::cout<< value<<std::endl;
+
+delete p;
+delete [] image;
+}
