d5173f711b95d6e180a322b4421fabe4acafc522
BorvizRobi <43295711+BorvizRobi@users.noreply.github.com>
2019-04-20 17:31:09 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,27 @@
-#include <iosteam>
+#include <iostream>
 #include "mlp.hpp"
 #include <png++/png.hpp>
 
-int main( int argc, char **argv)
+int main (int argc, char **argv)
 {
+    png::image <png::rgb_pixel> png_image (argv[1]);
 
-png::image <png::rgb_pixel> png_image (argv[1]);
-
-int size = png_image.get_widith() *png_image.get_height();
-
-Perceptron* p =new Perceptron(3,size,256,1);
-
-double* image = new double[size];
-
-for(int i{0}; i<png_image.get_width();++i)
-  for(int j{0}; i<png_image.get_width();++j)
-    image[i*png_image.get_width()+j]=png_image[i][j].red;
-
-double value =(*p)(image);
-
-std::cout<< value<<std::endl;
-
-delete p;
-delete [] image;
+    int size = png_image.get_width() * png_image.get_height();
+    
+    Perceptron* p = new Perceptron (3, size, 256, 1);
+    
+    double* image = new double[size];
+    
+    for (int i = 0; i<png_image.get_width(); ++i)
+        for (int j = 0; j<png_image.get_height(); ++j)
+            image[i*png_image.get_width() + j] = png_image[i][j].red;
+    
+    double value = (*p) (image);
+    
+    std::cout << value << std::endl;
+    
+    delete p;
+    delete [] image;
+    
 }
+
